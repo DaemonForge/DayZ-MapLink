80bdd1e452678ee484fc4690e37fbe778e3a2b8f
Daemon Forge <kevhoddinott@gmail.com>
2021-04-28 20:32:05 -0400

    Cleaning Up Mission Server and Changing to use config instead of hardcoded Values

--- MapLink/scripts/5_Mission/mission/MissionServer.c
+++ MapLink/scripts/5_Mission/mission/MissionServer.c
@@ -3,34 +3,34 @@ modded class MissionServer extends MissionBase
 	ref map<string, ref PlayerDataStore> m_PlayerDBQue = new map<string, ref PlayerDataStore>;
 	string m_worldname;
 	int MapLinkConfigRefreshTimer = 0;
-	const int MAPLINK_CONFIG_REFRESH_TIME = 90000;
+	static int MAPLINK_CONFIG_REFRESH_TIME = 90000;
 	
 	override void OnMissionStart(){
 		super.OnMissionStart();
 		GetGame().GetWorldName(m_worldname);
-		Print("[UAPI] On Mission Start " + m_worldname);
+		Print("[UAPI] On Mission Start World: " + m_worldname + " Server: " + UApiConfig().ServerID);
 	}
 	
 	
 	void LoadPlayerFromUApiDB(int cid, int status, string oid, string data){	
       	if (status == UAPI_SUCCESS){  //If its a success
-			Print("[UAPI] LoadPlayerFromDB - Success ID:" + cid + " - GUID: " + oid );
 			PlayerDataStore dataload;
 			if (UApiJSONHandler<PlayerDataStore>.FromString(data, dataload)){
-				Print("[UAPI] UApiJSONHandler - Success");
+				Print("[UAPI] LoadPlayerFromDB - Success ID:" + cid + " - GUID: " + oid );
 				if (dataload.IsValid() && dataload.GUID == oid){
-					Print("[UAPI] UApiJSONHandler - IsValid");
 					m_PlayerDBQue.Set(oid, PlayerDataStore.Cast(dataload));
 				} else if (m_PlayerDBQue.Contains(oid)) {
-					Print("[UAPI] m_PlayerDBQue - Remove");
 					m_PlayerDBQue.Remove(oid);
 				}
+				return;
 			}
-      	} else if (status == UAPI_EMPTY){
-			Print("[UAPI] LoadPlayerFromDB - Empty Response ID:" + cid + " - GUID: " + oid );
-		} else { //could do else if to catch different kinds of errors
-         	Print("[UAPI] api call failed");
-      	}
+			Print("[UAPI] LoadPlayerFromDB - Error Loading Data from the API - ID:" + cid + " - GUID: " + oid);
+      	} 
+		if (status == UAPI_EMPTY){
+			Print("[UAPI] LoadPlayerFromDB - Empty Response - ID:" + cid + " - GUID: " + oid );
+			return;
+		}
+        Error("[UAPI] LoadPlayerFromDB - API CALL FAILED - CHECK OVER YOUR CONFIGS AND ENSURE THAT THE API IS RUNNING");
 	}
 
 	override void OnClientPrepareEvent(PlayerIdentity identity, out bool useDB, out vector pos, out float yaw, out int preloadTimeout)
@@ -41,33 +41,14 @@ modded class MissionServer extends MissionBase
 			GetMapLinkConfig().Load();
 		}
 		if (identity){
-			Print("[UAPI] OnClientPrepareEvent  - " + preloadTimeout + " - Identity = " + identity.GetId());
 			int cid = UApi().db(PLAYER_DB).Load("TheHive", identity.GetId(), this, "LoadPlayerFromUApiDB");	
-			Print("[UAPI] db Load - ID:" + cid + " - GUID: " + identity.GetId() );
+			Print("[UAPI] DB Load - ID:" + cid + " - GUID: " + identity.GetId() );
 			//NotificationSystem.SimpleNoticiation(" Requesting you're login player Data From the API", "Notification","Notifications/gui/data/notifications.edds", -16843010, 10, identity);
-		} else {
-			Print("[UAPI] OnClientPrepareEvent - Identity = NULL");
 		}
-		if (GetHive()) { // use character from database
-			useDB = true;
-		} else {
-			// use following data without database
-			useDB = false;
-			pos = "1189.3 0.0 5392.48";
-			yaw = 0;
-		}
+		super.OnClientPrepareEvent(identity, useDB, pos, yaw, preloadTimeout);
 	}
 	
-	override void OnEvent(EventType eventTypeId, Param params) {
-		
-		if (eventTypeId == ClientPrepareEventTypeID){Print("[UAPI] OnEvent ClientPrepareEventTypeID");}
-		if (eventTypeId == ClientNewEventTypeID){Print("[UAPI] OnEvent ClientNewEventTypeID");}
-		if (eventTypeId == ClientReadyEventTypeID){Print("[UAPI] OnEvent ClientReadyEventTypeID");}
-		if (eventTypeId == ClientRespawnEventTypeID){Print("[UAPI] OnEvent ClientRespawnEventTypeID");}	
-		if (eventTypeId == ClientReconnectEventTypeID){Print("[UAPI] OnEvent ClientReconnectEventTypeID");}
-		if (eventTypeId == ClientDisconnectedEventTypeID){Print("[UAPI] OnEvent ClientDisconnectedEventTypeID");}
-		if (eventTypeId == LogoutCancelEventTypeID){Print("[UAPI] OnEvent LogoutCancelEventTypeID");}
-		
+	override void OnEvent(EventType eventTypeId, Param params) {		
 		if (eventTypeId == ClientNewEventTypeID) {
 			ClientNewEventParams newParams;
 			Class.CastTo(newParams, params);
@@ -84,41 +65,41 @@ modded class MissionServer extends MissionBase
 		
 	bool UApiOnClientNewEvent(PlayerIdentity identity, vector pos, ParamsReadContext ctx)
 	{
-		Print("[UAPI] UApiOnClientNewEvent - " + identity.GetId());
 		PlayerDataStore playerdata;		
 		if (identity && m_PlayerDBQue.Find(identity.GetId(), playerdata) && playerdata.IsValid()) {
 			pos = "0 0 0";
-			Print("Spawning player on " + m_worldname);
-			if (playerdata.m_Map != m_worldname && playerdata.m_TransferPoint == "") {
-				UApiServerData serverData;
-				if (playerdata.m_Map == "enoch") {
-					serverData = new UApiServerData("192.95.50.50", 2632, "daemon");
-				}
-				if (playerdata.m_Map == "chernarusplus") {
-					serverData = new UApiServerData("192.95.50.50", 2602, "");
-				}
-				if (playerdata.m_Map == "namalsk") {
-					serverData = new UApiServerData("192.95.50.50", 2662, "daemon");
-				}
-				NotificationSystem.Create(new StringLocaliser("Notification"),new StringLocaliser(" Redirecting to the correct server - " + playerdata.m_Map), "Notifications/gui/data/notifications.edds", -16843010, 10, identity);
+			vector ori = "0 0 0";
+			UApiServerData serverData;
+			Print("[UAPI] Spawning player " + identity.GetId() + " on: " + UApiConfig().ServerID + " World: " + m_worldname);
+			if (playerdata.m_Server != UApiConfig().ServerID && playerdata.m_TransferPoint == "") {
+				serverData = UApiServerData.Cast(GetMapLinkConfig().GetServer(playerdata.m_Server));
+				NotificationSystem.Create(new StringLocaliser("Map Link"),new StringLocaliser(" Redirecting to the correct server - " + playerdata.m_Server), "set:maplink_icons image:redirect", -16843010, 16, identity);
 				GetRPCManager().SendRPC("TheHive", "RPCRedirectedKicked", new Param1<UApiServerData>(serverData), true, identity);
+				Print("[UAPI] Player " + identity.GetId() + " Redirected to correct server " +  playerdata.m_Server);
 				m_PlayerDBQue.Remove(identity.GetId());
 				return false;
-			} 
-			if (playerdata.m_TransferPoint != "") {
-				pos = GetDayZGame().HiveGetSpawnLocation(m_worldname, playerdata.m_TransferPoint);
+			}
+			if (playerdata.m_Server != UApiConfig().ServerID && playerdata.m_TransferPoint != "") {
+				MapLinkSpawnPointPos pointPos;
+				if (!Class.CastTo(pointPos, GetMapLinkConfig().SpawnPointPos(playerdata.m_TransferPoint))){
+					serverData = UApiServerData.Cast(GetMapLinkConfig().GetServer(playerdata.m_Server));
+					NotificationSystem.Create(new StringLocaliser("Map Link"),new StringLocaliser(" Error On Connect This server isn't set up correctly sending you back to your orginal server - " + playerdata.m_Server), "set:maplink_icons image:redirect", -16843010, 16, identity);
+					GetRPCManager().SendRPC("TheHive", "RPCRedirectedKicked", new Param1<UApiServerData>(serverData), true, identity);
+					m_PlayerDBQue.Remove(identity.GetId());
+					Print("[UAPI] Error Server isn't set up to receive this arrival point Player " + identity.GetId() + " Redirected back to previous server " +  playerdata.m_Server);
+					return false;
+				}
+				pos = pointPos.Get();
+				ori = pointPos.GetOrientation();
 			}
 
-			Print("[UAPI] OnClientNewEvent - CreateWithIdentity - " + identity.GetId());
 			PlayerBase player = PlayerBase.Cast(PlayerDataStore.Cast(playerdata).CreateWithIdentity(PlayerIdentity.Cast(identity), pos));
-			Print("[UAPI] Identity Being Selected: " + identity.GetId());
 			GetGame().SelectPlayer(identity, player);
 			InvokeOnConnect(player, identity);
 			SyncEvents.SendPlayerList();
 			ControlPersonalLight(player);
 			SyncGlobalLighting(player);
-			PlayerDataStore.Cast(playerdata).SetupPlayer(player, pos);
-			Print("[UAPI] m_PlayerDBQue - Remove");
+			PlayerDataStore.Cast(playerdata).SetupPlayer(player, pos, ori);
 			GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).CallLater(player.SavePlayerToUApi, 100);
 			m_PlayerDBQue.Remove(identity.GetId());
 			return true;
