3c441f2cdc8d4cd0b7ebc9454f9693e0826f3a02
Daemon Forge <kevhoddinott@gmail.com>
2021-05-04 01:14:46 -0400

    Adding some functions to DayZGame
     Adding some functions to DayZGame to tell if its transferring so main menus can act accordingly

--- MapLink/scripts/3_Game/DayZGame.c
+++ MapLink/scripts/3_Game/DayZGame.c
@@ -4,25 +4,39 @@ modded class DayZGame
 	ref MapLinkCountTimerWidget m_MapLinkCountTimerWidget;
 	
 	autoptr UApiServerData ConnectTo;
+	bool m_IsToTransfer = false;
 	
 	void DayZGame() {
+		
 	}
 
+	
+	bool IsToTransfer(){
+		return ( m_IsToTransfer && ConnectTo && ConnectTo.IP != "" );
+	}
+	
+	string TransferToServerName(){
+		if (ConnectTo){
+			return ConnectTo.Name;
+		}
+		return "ERROR";
+	}
+	
 	override void DeferredInit() {
 		super.DeferredInit();
 	}
 	
 	void HiveSetReconnectTo(UApiServerData data){
-		Print("HiveSetReconnectTo");
 		ConnectTo = UApiServerData.Cast(data);
-		Print(ConnectTo);
+		if (ConnectTo){
+			m_IsToTransfer = true;
+		}
 	}	
 	
 	
 	void HiveDoReconnect(UIScriptedMenu menu){
-		Print("HiveDoReconnect");
-		Print(ConnectTo);
-		if (ConnectTo){
+		if (ConnectTo && m_IsToTransfer){
+			m_IsToTransfer = false;
 			GetGame().Connect(menu, ConnectTo.IP, ConnectTo.Port, ConnectTo.Password);
 			ConnectTo = NULL;
 		}
@@ -30,15 +44,14 @@ modded class DayZGame
 	
 	
 	void MapLinkStartCountDown(int time){
-		Print("[MAPLINK] MapLinkStartCountDown " + time);
 		if(!GetGame().IsClient()){return;}
 		if (!m_MapLinkCountTimerWidget){
 			m_MapLinkCountTimerWidget = new MapLinkCountTimerWidget;
 		}
 		m_MapLinkCountTimerWidget.Start(time);
 	}
+	
 	void MapLinkStopCountDown(){
-		Print("[MAPLINK] MapLinkStopCountDown");
 		if(!GetGame().IsClient()){return;}
 		if (m_MapLinkCountTimerWidget){
 			m_MapLinkCountTimerWidget.Stop();
