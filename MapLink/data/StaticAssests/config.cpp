de2e18c55a9887147996cef1672407d5e637f641
Dumpgrah <Andrewthompson0200@gmail.com>
2022-01-06 11:55:36 -0800

    Moved over Static Items including Boards and Boat and Heli
    Added
    Static Heli
    Static Boat
    Boarding Boards
    Redid the pathing for all textures and Models
    Made a Config cpp
    Added folder structure for models and textures

---
+++ MapLink/data/StaticAssests/config.cpp
@@ -0,0 +1,63 @@
+class CfgPatches
+{
+	class Hive_Statics
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"DZ_Data"
+		};
+	};
+};
+class CfgVehicles
+{
+	class HouseNoDestruct;
+	class GN_Hive_Boat : HouseNoDestruct
+	{
+		scope=2;
+		displayName = "Testing";
+		descriptionShort = "Testing";
+		model="\MapLink\data\StaticAssests\StaticAssests\Boat.p3d";
+	};
+	class Hive_Small_Boarding_Plank : HouseNoDestruct
+	{
+		scope=2;
+		displayName = "Testing";
+		descriptionShort = "Testing";
+		model="\MapLink\data\StaticAssests\StaticAssests\ShortPlank.p3d";
+		physLayer = "item_large";
+		itemBehaviour = 2;
+	};
+	class Hive_Medium_Boarding_Plank : HouseNoDestruct
+	{
+		scope=2;
+		displayName = "Testing";
+		descriptionShort = "Testing";
+		model="\MapLink\data\StaticAssests\StaticAssests\MediumPLank.p3d";
+		physLayer = "item_large";
+		itemBehaviour = 2;
+	};
+	class Hive_Long_Boarding_Plank : HouseNoDestruct
+	{
+		scope=2;
+		displayName = "Testing";
+		descriptionShort = "Testing";
+		model="\MapLink\data\StaticAssests\StaticAssests\LongPlank.p3d";
+		physLayer = "item_large";
+		itemBehaviour = 2;
+	};
+	class HiveStaticHeli: HouseNoDestruct
+	{
+		scope=2;
+		displayName="";
+		descriptionShort="";
+		model="\MapLink\data\StaticAssests\StaticAssests\StaticHeli.p3d";
+		forceFarBubble="false";
+		carveNavmesh=1;
+		rotationFlags=2;
+		itemSize[]={1,1};
+		weight=1000000;
+	};
+};
